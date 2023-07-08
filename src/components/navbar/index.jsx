19fc3cb282e9bfd346e5e59d6dc6fd332daa124b
nonwor <nonwor@gmail.com>
2023-07-08 15:27:06 -0700

    first

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+
+import 'bootstrap/dist/css/bootstrap.css';
+import { Navbar, Nav, NavDropdown, Container} from 'react-bootstrap';
+import { Link, useNavigate} from 'react-router-dom';
+
+const NavigateBar = () =>{
+
+    const navigate = useNavigate()
+
+    const handleLogoutAction =()=>{
+
+    }
+
+    return(
+        <>
+            <Navbar bg="light" expand="lg">
+                    <Container>
+                        <Navbar.Brand as={Link} to="/home">BluewaterMLabs</Navbar.Brand>
+                        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                        <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link as={Link} to="/upload">Upload Images</Nav.Link>
+                            <Nav.Link as={Link} to="/analysis">Data Analysis</Nav.Link>
+                        </Nav>
+                        
+                        <Nav className='logged-in-view'>
+                            
+                            <NavDropdown title="Profile" id="basic-nav-dropdown" className="ml-auto">
+                                <NavDropdown.Item as={Link} to='/userprofile'> Settings </NavDropdown.Item>
+                                
+                                <NavDropdown.Item href="#action/3.2"> Data </NavDropdown.Item>
+                                
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item onClick={handleLogoutAction}>
+                                    Log Out
+                                </NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+        </>
+    )
+}
+
+export default NavigateBar
---
+++ src/pages/analysis/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Analysis =()=>{
+    return(
+        <>
+            <h1>Analysis</h1>
+        </>
+    )
+}
+
+export default Analysis;
